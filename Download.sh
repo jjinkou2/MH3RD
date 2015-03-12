488d5ee66698b2fba578a0c1caeeec05f7be544f
jjinkou2 <jjinkou2@yahoo.fr>
2015-03-12 21:24:37 +0100

    linux

---
+++ Download.sh
@@ -0,0 +1,6 @@
+
+git pull
+
+cp MHP3RD.BIN /home/lili/.config/ppsspp/PSP/SAVEDATA/ULJM05800
+cp PARAM.SFO /home/lili/.config/ppsspp/PSP/SAVEDATA/ULJM05800
+
---
+++ Upload.sh
@@ -0,0 +1,7 @@
+cp /home/lili/.config/ppsspp/PSP/SAVEDATA/ULJM05800/MHP3RD.BIN .
+cp /home/lili/.config/ppsspp/PSP/SAVEDATA/ULJM05800/PARAM.SFO .
+
+git add *.* 
+git commit -m 'save'
+git push
+
