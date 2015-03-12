eab0795b48a8e390e7ad104ba30384b8a57a673a
jjinkou2 <jjinkou2@yahoo.fr>
2015-03-12 15:26:19 +0100

    'save'

--- download.bat
+++ download.bat
@@ -1,14 +1,14 @@
 @echo OFF
 git.exe pull
 
-echo " "
-echo "fichier recupere du serveur"
-echo "---------------------------"
+echo 
+echo fichier recupere du serveur
+echo ---------------------------
 
 copy /Y MHP3RD.BIN C:\Users\lyup7323\Documents\PPSSPP\PSP\SAVEDATA\ULJM05800
 
 copy /Y PARAM.SFO C:\Users\lyup7323\Documents\PPSSPP\PSP\SAVEDATA\ULJM05800
 
-echo "fichier copie dans le repertoire:"
-echo "---------------------------------"
-echo "C:\Users\lyup7323\Documents\PPSSPP\PSP\SAVEDATA\ULJM05800"
+echo fichier copie dans le repertoire:
+echo ---------------------------------
+echo C:\Users\lyup7323\Documents\PPSSPP\PSP\SAVEDATA\ULJM05800
--- upload.bat
+++ upload.bat
@@ -1,11 +1,11 @@
 @echo OFF
 copy /Y C:\Users\lyup7323\Documents\PPSSPP\PSP\SAVEDATA\ULJM05800\*.* .
-echo "fichier copie dans le repertoire"
-echo "--------------------------------"
+echo fichier copie dans le repertoire
+echo --------------------------------
 git.exe add *.* 
 git.exe commit -m 'save'
 git.exe push
-echo " "
-echo " "
-echo "fichier mis sur le serveur"
-echo "--------------------------"
+echo  
+echo  
+echo fichier mis sur le serveur
+echo --------------------------
