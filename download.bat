8b3501a639d036018f01bfb7240f5c3ec606fb2d
jjinkou2 <jjinkou2@yahoo.fr>
2015-03-12 14:52:22 +0100

    download

---
+++ download.bat
@@ -0,0 +1,11 @@
+@echo OFF
+git.exe pull
+
+echo " "
+echo "fichier recupere du serveur"
+echo "---------------------------"
+
+copy test1.txt C:\Users\lyup7323\Documents\PPSSPP\PSP\SAVEDATA\ULJM05800
+
+echo "fichier copie dans le repertoire"
+echo "--------------------------------"
